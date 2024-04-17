f89cd61c4ca9fc289d86994453399fdac38fb6d5
Demyan <demuanivanytsky8@gmail.com>
2024-04-17 13:28:21 +0300

    fix for deploy

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 
 import Form from "@components/Form";
@@ -54,13 +54,15 @@ const UpdatePrompt = () => {
   };
 
   return (
-    <Form
+    <Suspense>
+      <Form
       type='Edit'
       post={post}
       setPost={setPost}
       submitting={submitting}
       handleSubmit={updatePrompt}
     />
+    </Suspense>
   );
 };
 
